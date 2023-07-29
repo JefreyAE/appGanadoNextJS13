f2fd41a888d53005382f82ea065066b55465e268
Jefrey Arias Esquivel <jefariasj@gmail.com>
2023-07-29 15:06:48 -0600

    Fix favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,22 +4,42 @@ import { Inter } from 'next/font/google'
 import Footer from './layouts/footerComponent/Footer'
 import  UserProvider from '../contexts/userContext'
 import CentralContainer from './layouts/containersComponent/CentralContainer'
+import { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Registro',
   description: 'Registro ganadero',
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: 'favicon/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: 'favicon/favicon-16x16.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: 'favicon/apple-touch-icon.png',
+    },
+  ],
 }
 
 export default function RootLayout({ children, }: { children: React.ReactNode }) {
   return (
       <html lang="en">
-        <head>
-          <title>App ganadera</title>
+        <head> 
+          <link rel="icon" href="favicon/favicon.ico" />
+          <Script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" crossOrigin="anonymous" defer></Script>
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" crossOrigin="anonymous" as="style" />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"></link>
-          <Script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" crossOrigin="anonymous" defer></Script>
           <Script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" crossOrigin="anonymous" defer></Script>
         </head>
         <body className={inter.className}>
